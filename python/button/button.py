99689883a75f5e4add1e0eba26f1582121d4512f
Demo User <debian@beaglebone.localdomain>
2023-10-30 03:19:40 +0000

    Issues with previous code

--- python/button/button.py
+++ python/button/button.py
@@ -3,7 +3,7 @@
 Button Driver
 --------------------------------------------------------------------------
 License:   
-Copyright 2021-2023 - Sahana Prasanna
+Copyright 2021-2023 - Kevin Li
 
 Redistribution and use in source and binary forms, with or without 
 modification, are permitted provided that the following conditions are met:
